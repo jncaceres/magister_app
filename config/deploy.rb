acc63240e89b2c584dc7772942f421196cda0371
haroldmuller <hlmuller@uc.cl>
2017-01-27 12:55:34 -0300

    arreglos

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.1"
 
 set :application, "magister_app"
-set :repo_url, "git@github.com:pmespinosa/plataforma_flip.git"
+set :repo_url, "git@github.com:haroldmuller/magister_app.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
