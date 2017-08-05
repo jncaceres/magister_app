eec2ce8d3df3dced29e2c67ba252502388e9d115
Daniel Salazar <disalazarg@gmail.com>
2017-08-05 12:53:08 -0400

    Fix for replies form

--- config/initializers/redcarpet.rb
+++ config/initializers/redcarpet.rb
@@ -1,2 +1,2 @@
 renderer  = Redcarpet::Render::HTML.new(render_options = {})
-$markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+$markdown = Redcarpet::Markdown.new(renderer, extensions = {autolink: true, tables: true})
