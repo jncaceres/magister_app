34fa639e181c6e3f53ca1d3b9e349c7cc3ce55b8
Tomás <tomvalenz@gmail.com>
2019-01-03 22:23:32 -0300

    principio tecleras 3

--- app/controllers/tecleras_controller.rb
+++ app/controllers/tecleras_controller.rb
@@ -2,4 +2,11 @@ class TeclerasController < ApplicationController
   def index
     @tecleras = Teclera.all
   end
+
+  def show
+  end
+
+  def new
+    @teclera = Teclera.new
+  end
 end
--- app/models/teclera.rb
+++ app/models/teclera.rb
@@ -1,3 +1,2 @@
 class Teclera < ActiveRecord::Base
-  has_many :users
 end
