23114fd10c79bd3912e73ad89de20df45998c43a
joaquin <jncaceres@uc.cl>
2019-07-16 16:12:40 -0400

    cambios lti

--- app/controllers/lti_controller.rb
+++ app/controllers/lti_controller.rb
@@ -5,16 +5,19 @@ class LtiController < ApplicationController
             render "lti/launch_error", status: 401
             return
         end 
-
+        condicion_1 = params[:lti_message_type] == "basic-lti-launch-request"
+        condicion_2 = params[:lti_version] == "LTI-1p0"
+        condicion_3 = params[:resource_link_id]
+        
         require 'oauth/request_proxy/rack_request'
         @provider = IMS::LTI::ToolProvider.new(
           params[:oauth_consumer_key],
           Rails.configuration.lti_settings[params[:oauth_consumer_key]],
           params
         )
-        
+        render_plain: params.sort
 
-        if not @provider.valid_request?(params)
+        if not @provider.valid_request?(request)
           # the request wasn't validated
           render "lti/launch_error", status: 401
           return
