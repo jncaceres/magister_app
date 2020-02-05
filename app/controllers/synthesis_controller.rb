1382e0b8dda0485aeec65a89d5b22ba166ea63f0
Seba Velozo <savelozo@uc.cl>
2020-02-04 23:51:40 -0300

    Fix synthesis controller for show two argues and edit list in show

--- app/controllers/synthesis_controller.rb
+++ app/controllers/synthesis_controller.rb
@@ -63,12 +63,8 @@ class SynthesisController < ApplicationController
     @course = Course.find_by_id(id_curso)
     @course.users.each do |alumno|
       @corregido = User.find_by_id(alumno.id)
-      @corrector = User.find_by_id(alumno.corrector)
       if alumno.role == "alumno" and alumno.answers.find_by_homework_id(@homework.id)
         @my_answer = @corregido.answers.find_by_homework_id(@homework.id)
-        if alumno.corrector
-          @partner_answer = @corrector.answers.find_by_homework_id(@homework.id)
-        end
         if @homework.actual_phase == "responder"
           if @my_answer.responder.nil?
             @my_answer.responder = ""
@@ -76,26 +72,22 @@ class SynthesisController < ApplicationController
           answer << @my_answer.responder.to_s
           answer << "\n"
         elsif @homework.actual_phase == "argumentar"
-          if @course.course_type == "Resumen"
-            if @partner_answer
-              if @partner_answer.responder.nil?
-                @partner_answer.responder = ""
-              end
-              answer << @partner_answer.responder.to_s
-              answer << "\n"
+          if @my_answer.argumentar != nil or @my_answer.argumentar_2 != nil
+            if @my_answer.argumentar.nil?
+              answer << " "
             else
-              answer << "No se ha generado ninguna respuesta"
+              answer << @my_answer.argumentar.to_s
             end
+            answer << "\n"
+
+            if @my_answer.argumentar_2.nil?
+              answer << " "
+            else
+              answer << @my_answer.argumentar_2.to_s
+            end
+            answer << "\n"
           else
-            if @partner_answer
-              if @partner_answer.argumentar.nil?
-                @partner_answer.argumentar = ""
-              end
-              answer << @partner_answer.argumentar.to_s
-              answer << "\n"
-            else
-              answer << "No se ha generado ninguna respuesta"
-            end
+            answer << "No se ha generado ningún argumento para esta respuesta"
           end
         elsif @homework.actual_phase == "rehacer"
           if @my_answer.rehacer.nil?
